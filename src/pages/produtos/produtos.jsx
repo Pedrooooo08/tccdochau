85ce9c98ac8ab6ba27985ff556e41fa250cf5a42
pedroooooo08 <pedrohenriquebarros89@gmail.com>
2024-10-29 17:45:31 -0300

    swiper

--- src/pages/produtos/produtos.jsx
+++ src/pages/produtos/produtos.jsx
@@ -7,7 +7,7 @@ import combo from '../image/Combo de Festas.jpg';
 import Cabegalho from '../cabegalho/cabegalho';
 import Footer from '../footer/footer';
 
-break e foda
+
 
 const products = [
   {
