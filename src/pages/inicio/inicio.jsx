94393d40489b5504c17449b68b01c339f271a01e
Pedrooooo08 <pedrohenriquebarros89@gmail.com>
2024-10-31 11:27:31 -0300

    jk

--- src/pages/inicio/inicio.jsx
+++ src/pages/inicio/inicio.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import Cabecalho from '../cabegalho/cabegalho'; 
+import Cabecalho from '../cabegalho/cabegalho';
 import Sonho from '../image/7aadb69a-7915-4935-b1a6-635cd8e26193___4883223899b870a1c2244be6095659fb.webp';
 import salgado from '../image/756a78c2-7f59-499d-9979-a57a09ec92b7___eb262e7e3c6ef1fb6cb02df817ca952b.webp';
 import doces from '../image/tesãoi.webp';
 import morango from '../image/guloseimas.webp'
 import Footer from '../footer/footer';
-import ori  from '../image/salgado.jpg'
-import oris from '../image/morago.webp'
 
+import mel from '../image/podemel.webp'
+import brow from '../image/brownie.webp'
+import van from '../image/brisadeiro.webp'
+import bolo from '../image/trufas.webp'
 import './inicio.scss';
 
 
@@ -34,30 +36,58 @@ const INICIO = () => {
         className="swiper-container"
       >
         <SwiperSlide>
-          <img src={Sonho} alt="Sonho" className="swiper-image"/>
+          <img src={Sonho} alt="Sonho" className="swiper-image" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={salgado} alt="Salgado" className="swiper-image"/>
+          <img src={salgado} alt="Salgado" className="swiper-image" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={doces} alt="Doces Individuais" className="swiper-image"/>
+          <img src={doces} alt="Doces Individuais" className="swiper-image" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={morango} alt="Morango" className="swiper-image"/>
+          <img src={morango} alt="Morango" className="swiper-image" />
         </SwiperSlide>
       </Swiper>
-      <div className='hulk 1'>
-      <img src={ori} alt="image1" />
-      </div>
       <h1 className='titulo-principal'>Sabores da Rua</h1>
       <h2 className='subtitulo'>
+
         Transformando cada doce em uma celebração de sabores, onde a tradição se encontra com a inovação em cada fatia!
+
       </h2>
 
-     
+      <div className="produtos">
+        <h2>Nossos Produtos</h2>
+        <div className="produtos-container">
+          <div className="produto-item">
+            <img src={van} alt="Produto 1" />
+            <p>salgodo do breack
+              0,00rs
+            </p>
+          </div>
+          <div className="produto-item">
+            <img src={mel} alt="Produto 2" />
+            <p>salgodo do breack
+              0,00rs</p>
+          </div>
+          <div className="produto-item">
+            <img src={bolo} alt="Produto 3" />
+            <p>salgodo do breack
+              0,00rs</p>
+          </div>
+          <div className="produto-item">
+            <img src={brow} alt="Produto 4" />
+            <p>salgodo do breack
+              0,00rs</p>
+          </div>
+
+        </div>
+      </div>
+
+
+
       <Footer />
     </div>
   );
 };
-a
+
 export default INICIO;
